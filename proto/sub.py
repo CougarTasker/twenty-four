3601cddc9533569c726d0438c2217a5f54079a8d
XingruiGu <58687087+XingruiGu@users.noreply.github.com>
2020-02-14 11:17:38 +0000

    added detail to bg

--- proto/sub.py
+++ proto/sub.py
@@ -1,4 +1,8 @@
 from vect import Vector
+from background import Background
+from carol import Carol
+from pearl import Pearl
+from bubble import bubblesheet
 import random,math,time
 try:
 	import simplegui
@@ -10,44 +14,32 @@ CANVAS_WIDTH = 600
 CANVAS_HEIGHT = 400
 
 class Interaction:
-	def __init__(self,dimensions):
+	def __init__(self,dimensions,pearl, carol):
 		self.lastFrameTime = time.time()
 		self.dimensions = dimensions
-		self.clouds = simplegui.load_image("https://raw.githubusercontent.com/CougarTasker/twenty-four/master/proto/background_clouds.png")
+		self.background = Background(dimensions)
+		self.perl = pearl
+		self.carol = carol
 	def update(self):
 		pass
-	def background(self,canvas,pollycount,wavecount,frequency,height,waveheight,color):
-		path = [(0,CANVAS_HEIGHT)]
-		offset = self.lastFrameTime%(1/frequency)*frequency
-		for x in range(pollycount+1):
-			ang = x/pollycount*wavecount + offset
-			ang *= 2*math.pi
-			path.append((x/pollycount*CANVAS_WIDTH,((math.sin(ang)*waveheight/2+height)*CANVAS_HEIGHT)))
-		path.append((CANVAS_WIDTH,CANVAS_HEIGHT))
-		path.append((0,CANVAS_HEIGHT))
-		canvas.draw_polygon(path,1,color,color)
-	def looping_clouds(self,canvas,frequency):
-		offset = self.lastFrameTime%(1/frequency)*frequency
 	def draw(self, canvas):
 		delta = time.time()-self.lastFrameTime
 		self.lastFrameTime = time.time()
-		self.background(canvas,30,4,1,0.3,0.03,"rgb(0,0,100)")
-		self.background(canvas,30,3,-0.6,0.3,0.04,"rgb(0,0,150)")
-		self.background(canvas,50,2,0.2,0.3,0.05,"rgb(0,0,250)")
-		
-		
+		self.background.draw(canvas)
+		self.carol.draw_canvas(canvas)
+		self.perl.draw_canvas(canvas)
 
 
 
+pearl = Pearl(Vector(417,383))
+carol = Carol(Vector(136,331))
+i = Interaction((CANVAS_WIDTH, CANVAS_HEIGHT),pearl,carol)
 
-
-
-i = Interaction((CANVAS_WIDTH, CANVAS_HEIGHT))
-
+#bubble1 = bubblesheet(Vector())
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("ball-wall", CANVAS_WIDTH, CANVAS_HEIGHT)
 
-frame.set_canvas_background("rgb(120,120,256)")
+frame.set_canvas_background("rgb(87,150,250)")
 frame.set_draw_handler(i.draw)
 # Start the frame animation
 frame.start()
