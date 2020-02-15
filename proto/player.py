21aff6e0e3fb756578a13311f0824006573d1d89
Tiger-lily <tigerlilygoldsmith@gmail.com>
2020-02-15 19:46:14 +0000

    added getter and setter methods or player attributes

--- proto/player.py
+++ proto/player.py
@@ -6,38 +6,56 @@ except ImportError:
 
 
 class Player:
-    def __init__(self, dimensions):
-            self.canvas_dim = dimensions
+   def __init__(self, dimensions):
+      self.canvas_dim = dimensions
 
-            ##image info + dimensions (constants)
-            self.img = simplegui.load_image("https://raw.githubusercontent.com/CougarTasker/twenty-four/player/proto/images/boatman.png")
-            self.dim = (4096, 4096)
-            self.cen = (self.dim[0]/2, self.dim[1]/2)
-            self.draw_dim = (190, 190)
+      ##image info + dimensions (constants)
+      self.img = simplegui.load_image("https://raw.githubusercontent.com/CougarTasker/twenty-four/master/proto/images/boatman.png")
+      self.dim = (4096, 3790)
+      self.cen = (self.dim[0]/2, self.dim[1]/2)
+      self.draw_dim = (190, 190)
 
-            ##player position and other attributes (variables)
-            self.pos = Vector(self.draw_dim[0]/2,self.draw_dim[1]/2)
-            self.vel = Vector(0,0)
-            self.lives = 3
-            self.points = 0
-            #self.rod = Rod(xxx)
-            
+      ##player position and other attributes (variables)
+      self.pos = Vector(self.draw_dim[0]/2,self.draw_dim[1]/2+15)
+      self.vel = Vector(0,0)
+      self.lives = 3
+      self.points = 0
+      #self.rod = Rod(xxx)
 
+
+   def getPos(self):
+      return self.pos
+   def setPos(self, newPos):
+      self.pos = newPos
+
+   def getVel(self):
+      return self.vel
+   def addVel(self, velocity):
+      self.vel.add(velocity)
+
+   def getLives(self):
+      return self.lives
+   def setLives(self, life):
+      self.lives = life
+   
+   def setToZero(self):
+      self.points = 0
+   def setPoints(self, newP):
+      self.points = newP
+   def getPoints(self):
+      return self.points
         
-        
-    def draw(self,canvas):
-        canvas.draw_image(self.img, self.cen, self.dim, self.pos.get_p(), self.draw_dim) 
-        #canvas.draw_image(image, center_source, width_height_source, center_dest, width_height_dest)
-        
-                
-def draw(canvas):
-    play = Player((900,400))
-
-    play.draw(canvas)
-
-frame = simplegui.create_frame("ship test", 600,400)
-
-
-frame.set_draw_handler(draw)
-# Start the frame animation
-frame.start()
+   def draw(self,canvas):
+       canvas.draw_image(self.img, self.cen, self.dim, self.pos.get_p(), self.draw_dim) 
+                        
+##def draw(canvas):
+##    play = Player((900,400))
+##
+##    play.draw(canvas)
+##
+##frame = simplegui.create_frame("ship test", 600,400)
+##
+##
+##frame.set_draw_handler(draw)
+### Start the frame animation
+##frame.start()
