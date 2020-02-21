34a10e27e48acab61903841404dd5f5bb47b3197
Tiger-lily <tigerlilygoldsmith@gmail.com>
2020-02-21 10:34:38 +0000

    removed comments for background elements

--- proto/start.py
+++ proto/start.py
@@ -1,8 +1,4 @@
 from vect import Vector
-# from carol import Carol
-# from pearl import Pearl
-# from bubble import bubblesheet
-
 from keyboard import Keyboard 
 from player import Player
 from background import Background as Bg
@@ -19,11 +15,9 @@ CANVAS_WIDTH = 1000
 CANVAS_HEIGHT = round(CANVAS_WIDTH*9/16)
 
 class Interaction:
-   def __init__(self,dimensions, kbd):#,pearl, carol):
+   def __init__(self,dimensions, kbd):
       self.lastFrameTime = time.time()
       self.dimensions = dimensions
-      # self.perl = pearl
-      # self.carol = carol
       self.back = Bg(dimensions)
       self.fish = School(30,(CANVAS_WIDTH, CANVAS_HEIGHT))
       self.player = Player(dimensions)
@@ -45,17 +39,12 @@ class Interaction:
       delta = time.time()-self.lastFrameTime
       self.lastFrameTime = time.time()
       self.back.draw(canvas)
-      # self.carol.draw_canvas(canvas)
-      # self.perl.draw_canvas(canvas)
       self.fish.draw(canvas,delta)
       self.player.draw(canvas)
 
 
-#pearl = Pearl(Vector(417,383))
-#carol = Carol(Vector(136,331))
-
 kbd = Keyboard()
-i = Interaction((CANVAS_WIDTH, CANVAS_HEIGHT),kbd)#,pearl,carol)
+i = Interaction((CANVAS_WIDTH, CANVAS_HEIGHT),kbd)
 
 
 # Create a frame and assign callbacks to event handlers
