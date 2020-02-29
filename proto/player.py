a1b1409c9d3cae2000bd5c00d8a95c9b64af8c7d
Xingrui Gu <zhah060@cim-ts-node-03.cc.rhul.local>
2020-02-29 14:40:37 +0000

    a little change

--- proto/player.py
+++ proto/player.py
@@ -1,6 +1,7 @@
 import os
 from vect import Vector
 from rod import Rod
+from fish import Shark
 try:
 	import simplegui
 except ImportError:
@@ -8,67 +9,81 @@ except ImportError:
 
 
 class Player:
-   def __init__(self, dimensions):
-      self.canvas_dim = dimensions
-      addr = os.getcwd()
-      
-      ##image info + dimensions (constants)
-      self.img = simplegui.load_image("file:///"+addr+"/images/colouredBoth.png")
-      self.dim = (4096, 4096)
-
-      self.cen = (self.dim[0]/2, self.dim[1]/2)
-      self.draw_dim = (140, 160)
-      self.y_offset = 45
-      #self.x_offset
+	def __init__(self, dimensions):
+		self.canvas_dim = dimensions
+		addr = os.getcwd()
+		##image info + dimensions (constants)
+		self.img = simplegui.load_image("file:///"+addr+"/images/colouredBoth.png")
+		self.dim = (4096, 4096)
+	
+		self.cen = (self.dim[0]/2, self.dim[1]/2)
+		self.draw_dim = (140, 160)
+		self.y_offset = 45
+		#self.x_offset
 
       ##player position and other attributes (variables)
-      self.pos = Vector(self.draw_dim[0]/2,self.draw_dim[1]/2+self.y_offset)
-      self.vel = Vector(0,0)
-      self.lives = 3
-      self.points = 0
+		self.pos = Vector(self.draw_dim[0]/2,self.draw_dim[1]/2+self.y_offset)
+		self.vel = Vector(0,0)
+		self.lives = 3
+		self.points = 0
+		self.fishscore = 5
+		self.overpoint = 100 #* numberoflevel
       #self.rod = Rod(radius, "file:///"+addr+"/images/hook.png", "file:///"+addr+"/images/colouredBoth.png")
 
 
-   def getPos(self):
-      return self.pos
-   def setPos(self, newPos):
-      self.pos = newPos
+	def getPos(self):
+		return self.pos
+	def setPos(self, newPos):
+		self.pos = newPos
 
-   def getVel(self):
-      return self.vel
-   def addVel(self, velocity):
-      self.vel.add(velocity)
+	def getVel(self):
+		return self.vel
+	def addVel(self, velocity):
+		self.vel.add(velocity)
 
-   def getLives(self):
-      return self.lives
-   def setLives(self, life):
-      self.lives = life
+	def getLives(self):
+		return self.lives
+	def setLives(self, life):
+		self.lives = life
    
-   def setToZero(self):
-      self.points = 0
-   def setPoints(self, newP):
-      self.points = newP
-   def getPoints(self):
-      return self.points
+	def setToZero(self):
+		self.points = 0
+	def setPoints(self, newP):
+		self.points = newP
+	def getPoints(self):
+		return self.points
 
-   def update(self):
-      self.pos.add(self.vel)
-      self.vel.multiply(0.85)
-      #print(self.pos.get_p())
+	def update(self):
+		self.pos.add(self.vel)
+		self.vel.multiply(0.85)
+		#print(self.pos.get_p())
       
-   def inBounds(self):
-      return ((0 <= self.pos.get_p()[0]) and (self.canvas_dim[0] >= self.pos.get_p()[0]))
+	def inBounds(self):
+		return ((0 <= self.pos.get_p()[0]) and (self.canvas_dim[0] >= self.pos.get_p()[0]))
 
-   
-   def set(self):
-      if (0 > self.pos.get_p()[0]):
-         self.pos = Vector(1,self.draw_dim[1]/2+self.y_offset)
-      else:
-         self.pos = Vector(self.canvas_dim[0]-1,self.draw_dim[1]/2+self.y_offset)
+	def loseheart(self,rod):
+		hasshark = False
+		if len(rod.catch) != 0:
+			for fish in rod.catch:
+				if type(fish)==Shark:
+					hasshark = True
+		if hasshark:
+			self.lives -= 1
+	def calculatescore(self,rod):
+		if len(rod.catch) != 0:
+			if rod.hasshark:
+				self.points += (len(rod.catch)-1) * self.fishscore
+			else:
+				self.points += len(rod.catch) * self.fishscore
+	def set(self):
+		if (0 > self.pos.get_p()[0]):
+			self.pos = Vector(1,self.draw_dim[1]/2+self.y_offset)
+		else:
+			self.pos = Vector(self.canvas_dim[0]-1,self.draw_dim[1]/2+self.y_offset)
       
         
-   def draw(self,canvas):
-       canvas.draw_image(self.img, self.cen, self.dim, self.pos.get_p(), self.draw_dim) 
+	def draw(self,canvas):
+		canvas.draw_image(self.img, self.cen, self.dim, self.pos.get_p(), self.draw_dim) 
                         
 ##def draw(canvas):
 ##    play = Player((900,400))
