4d5ba67d969b81047e7622949d146c880c4e0300
Cougar Tasker <cougartasker@outlook.com>
2020-03-05 22:28:24 +0000

    hearts tweek

--- proto/hearts.py
+++ proto/hearts.py
@@ -8,7 +8,7 @@ from vect import Vector
 ##class to draw the hearts onto the screen for the player lives
 
 class Hearts:
-	def __init__(self, dimensions,time,die):
+	def __init__(self,dimensions,time,die):
 		self.die = die
 		self.canvas_dim = dimensions
 		addr = os.getcwd()
@@ -16,10 +16,11 @@ class Hearts:
 		self.img_full = simplegui.load_image("file:///"+addr+"/images/fullheart.png")
 		self.img_empty = simplegui.load_image("file:///"+addr+"/images/emptyheart.png")
 		self.dim = (51,42)
+		self.size = 0.65
 		self.cen = (self.dim[0]/2, self.dim[1]/2)
-		self.draw_dim = (self.dim[0]/2, self.dim[1]/2)
-		self.pos = Vector(self.dim[0]/2, self.dim[1]/2)
-		self.offset = 25
+		self.draw_dim = (self.dim[0]*self.size, self.dim[1]*self.size)
+		self.pos = Vector(self.dim[0]*self.size, self.dim[1]*self.size)
+		self.offset = self.draw_dim[0]
 		self.lives_max = 3
 		self.lives = self.lives_max
 	def resetLives(self):
@@ -30,14 +31,14 @@ class Hearts:
 		self.lives -= 1 
 		if self.lives <= 0:
 			self.die.gameover()
-		
-
 	def draw(self,canvas):
+		if self.lives < 0:
+			self.lives = 0
 		for i in range(0,self.lives):
-			self.drawheart(canvas, self.img_full, self.offset*i)
-		for i in range(0, self.lives_max-self.lives):
-			self.drawheart(canvas, self.img_empty, self.offset *(2-i))
-	def drawheart(self,canvas,img,offset):
-		canvas.draw_image(img, self.cen, self.dim, (self.pos.get_p()[0] +offset,self.pos.get_p()[1]),self.draw_dim)
+			self.drawheart(canvas, self.img_full, i)
+		for i in range(self.lives, self.lives_max):
+			self.drawheart(canvas, self.img_empty,i)
+	def drawheart(self,canvas,img,i):
+		canvas.draw_image(img, self.cen, self.dim, (self.pos+Vector(self.offset,0)*i).get_p(),self.draw_dim)
 		
 		
