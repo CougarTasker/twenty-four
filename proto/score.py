3a0c93da27f46a400bbdae04e42b126bb862b1d5
Cougar Tasker <cougartasker@outlook.com>
2020-03-08 11:38:13 +0000

    changed game time and added more decimal places

--- proto/score.py
+++ proto/score.py
@@ -3,7 +3,7 @@ class Score:
 	def __init__(self,time,dim,die,frame):
 		self.score = 0
 		self.time = time
-		self.maxtime = 9
+		self.maxtime = 99
 		self.starttime = self.time.time()
 		self.scoreh = 30
 		self.timeh = 25
@@ -17,7 +17,7 @@ class Score:
 		if t <= 0:
 			t = 0
 			self.die.gameOver()
-		return round(t)
+		return t
 	def resetScore(self):
 		self.score = 0
 	def incScore(self,score):
@@ -25,4 +25,4 @@ class Score:
 	def draw(self,canvas):
 		canvas.draw_polygon((self.pos.get_p(),(self.pos+self.size.x*Vector(1,0)).get_p(),(self.pos+self.size).get_p(),(self.pos+self.size.y*Vector(0,1)).get_p()),1,"rgba(255,255,255,0)","rgba(100,100,100,0.5)")
 		canvas.draw_text("Score: "+'{:04d}'.format(self.score), (self.pos+Vector(self.padding,self.scoreh+self.padding)).get_p(), self.scoreh, "orange")
-		canvas.draw_text("time: "+'{:02d}'.format(self.timeleft()), (self.pos+Vector(self.padding,self.scoreh+self.padding+self.timeh)).get_p(), 25, "red")
+		canvas.draw_text("time: "+'{:02.3f}'.format(self.timeleft()), (self.pos+Vector(self.padding,self.scoreh+self.padding+self.timeh)).get_p(), 25, "red")
