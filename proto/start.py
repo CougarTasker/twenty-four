77562e60e61d3e50a10a5a73e72b8aaa7b9de47b
Tiger-lily <tigerlilygoldsmith@gmail.com>
2020-04-05 22:15:56 +0100

    added comments to start file

--- proto/start.py
+++ proto/start.py
@@ -13,16 +13,20 @@ CANVAS_HEIGHT = round(CANVAS_WIDTH*9/16)
 
 kbd = Keyboard()
 addr = os.getcwd()
+#address
 
 
 # Create a frame and assign callbacks to event handlers
-frame = simplegui.create_frame("Fisherman's Catch", CANVAS_WIDTH, CANVAS_HEIGHT,0)
+frame = simplegui.create_frame("Fishing Game", CANVAS_WIDTH, CANVAS_HEIGHT,0)
 i = Interaction((CANVAS_WIDTH, CANVAS_HEIGHT),kbd,frame)
 
+#establish event handlers for the keyboard object
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
 
+#set frame draw handler as the interaction object draw method
 frame.set_canvas_background("rgb(87,150,250)")
 frame.set_draw_handler(i.draw)
+
 # Start the frame animation
 frame.start()
