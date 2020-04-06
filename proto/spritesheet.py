f5dc121d4f0482500f6fb61d611396fca7e44198
Tiger-lily <tigerlilygoldsmith@gmail.com>
2020-04-06 22:31:40 +0100

    removed unnecessary code

--- proto/spritesheet.py
+++ proto/spritesheet.py
@@ -44,24 +44,7 @@ class SpriteSheet(object):
 		canvas.draw_image(self.img,(self.cent+loc).get_p(),self.adim,center,size,rotation)
 		
 
-def draw(canvas):
-	anim.draw(canvas)
-class Clock:
-	def __init__(self):
-		self.time = 0
-	def tick(self):
-		self.time += 1
-	def transition(self,rate):
-		frame_duration = 1/rate
-		return self.time % frame_duration == 0
-
-
-if __name__ == "__main__":
-	anim = SpriteSheet("file:///C:/Users/Couga/Documents/repos/twenty-four/milestone4/runnerSheet.png",(6,5),Vector(WIDTH/2,HEIGHT/2),-1,1000,0.3)
-	frame = simplegui.create_frame('SpriteSheet', WIDTH, HEIGHT)
-	frame.set_canvas_background("red")
-	frame.set_draw_handler(draw)
-	frame.start()
+	
 
 
 		
