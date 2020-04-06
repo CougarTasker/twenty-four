bceacfba2048d80b2b543a00af7cb81c56efe99d
Tiger-lily <tigerlilygoldsmith@gmail.com>
2020-04-06 23:21:59 +0100

    added comments to player.py

--- proto/player.py
+++ proto/player.py
@@ -1,4 +1,3 @@
-
 import os
 from vect import Vector
 from rod import Rod
@@ -14,21 +13,20 @@ class Player:
 		self.time = time
 		self.lastFrameTime = self.time.time()
 		self.canvas_dim = dimensions
+
+		#address needed to gain local directory to access images folder below
 		addr = os.getcwd()
+		
 		##image info + dimensions (constants)
 		self.img = simplegui.load_image("file:///"+addr+"/images/boat2.png")
 		self.dim = (4096, 4096)
-	
 		self.cen = (self.dim[0]/2, self.dim[1]/2)
 		self.draw_dim = (140, 160)
 		self.y_offset = 80
-		#self.x_offset
-
-	  ##player position and other attrbutes (variables)
+		
+                ##player position and velocity
 		self.pos = Vector(self.draw_dim[0]/2,self.draw_dim[1]/5+self.canvas_dim[1]*0.3-self.y_offset)
 		self.vel = Vector(0,0)
-	  #self.rod = Rod(radius, "file:///"+addr+"/images/hook.png", "file:///"+addr+"/images/colouredBoth.png")
-
 
 	def getPos(self):
 		return self.pos
@@ -37,24 +35,31 @@ class Player:
 
 	def getVel(self):
 		return self.vel
+
+	#setter for velocity not needed, so replaced by add velocity method
 	def addVel(self, velocity):
 		self.vel.add(velocity)
+
+	#updates position based on velocity and frame rate	
 	def update(self):
 		delta = self.time.time()-self.lastFrameTime
 		self.lastFrameTime = self.time.time()
 		self.pos.add(self.vel*delta)
 		self.vel.multiply(0.85)
-		#print(self.pos.get_p())
-	  
+
+	#checks whether the user is within the horizontal bounds of the screen  
 	def inBounds(self):
 		return ((self.draw_dim[0]/2 <= self.pos.x) and (self.canvas_dim[0] - self.draw_dim[0]/2 >= self.pos.x))
 
+        #sets users positions when they are reaching outer bounds
 	def set(self):
 		self.vel *= -1
 		if (self.pos.get_p()[0]< self.canvas_dim[0]/2):
 			self.pos = Vector(self.draw_dim[0]/2+1,self.pos.y)
 		else:
 			self.pos = Vector(self.canvas_dim[0]-self.draw_dim[0]/2-1,self.pos.y)
+
+        #draws player ship sprite to canvas
 	def draw(self,canvas):
 		canvas.draw_image(self.img, self.cen, self.dim, self.pos.get_p(), self.draw_dim)
 								
